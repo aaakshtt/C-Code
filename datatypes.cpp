86988292b34be85aa328872a93db92f6ad109793
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-05-08 00:12:05 +0530

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int glo = 6;
+void sum(){
+	int a;
+	cout<<"first function\n"<<glo;
+	
+}
+
+int main() {
+	int glo = 9;
+	glo =78;
+//	int a = 6;
+//	int b =5;
+	
+	float pi = 3.14;
+	char  c = 'ud';
+	bool is_true = true;
+
+  cout<<"Hello World! This is Akshat and the sum is   "<<sum ; //We can ad  a coment over here 
+  cout<<"\n The value of pie is " <<pi;
+  cout<<"\nThe value of character q  is"<<c<<"\n";
+  cout<<"The value of glo is"<<glo<<"    "<<is_true;
+  return 0;
+  
+}
+
