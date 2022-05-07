2bf8642ebb2ff61d94cef72d3775d152b8e657f7
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-05-07 23:52:58 +0530

    Add files via upload

---
+++ code1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a = 6;
+	int b =5;
+	int sum = a+b;
+	float pi = 3.14;
+	char  c = 'u';
+  cout<<"Hello World! This is Akshat and the sum is   "<<sum ; //We can ad  a coment over here 
+  cout<<"\n The value of pie is " <<pi;
+  cout<<"\nThe value of character q  is"<<c;
+  return 0;
+  
+}
+
