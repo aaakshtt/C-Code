bc9de9ae42598ed899c811bfd218321656495762
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-05-08 00:36:01 +0530

    Create numberincrement.cpp

---
+++ numberincrement.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <climits>   // integer limits in header file
+using namespace std;
+//Checking the upper and lower limits
+int main(){
+	int num = 10;
+	cout<<"\nthe nuber is "<<num;
+	num++;
+	cout<<"\n The number after increment is  "<<num;
+	num++;
+	cout<<"\nThe number after another increment is  "<<num;
+	num--;
+	cout<<"\nThe number after subtracting 1 is"<<num;
+}
